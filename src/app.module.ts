6161e596e91ee6ab412ddd4eef27a3b35b453d5e
Keven Costa <ogkeven278@gmail.com>
2025-12-17 11:23:30 -0300

    chore: configurar ConfigModule e TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    // Deixa disponivel em todo o projeto as variaveis de hambiente.
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // configurar acesso ao banco de dados
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT ?? 3310),//alterar as configurações 
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    })
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }

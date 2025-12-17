640bc9979aac68612df997fe86adc475884159bb
Lucas Mafra <lucasmafra.med@gmail.com>
2025-12-17 18:31:49 -0300

    Adicionando Classe Colaborador

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-
+import { Colaborador } from './colaborador/entities/colaborador.entity';
+import { ColaboradorModule } from './colaborador/colaborador.module';
 
 @Module({
   imports: [
-    // Deixa disponivel em todo o projeto as variaveis de hambiente.
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
-    // configurar acesso ao banco de dados
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: +(process.env.DB_PORT ?? 3310),//alterar as configurações 
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [],
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_rh',
+      entities: [Colaborador],
       synchronize: true,
-    })
+    }),
+    ColaboradorModule,
   ],
   controllers: [],
   providers: [],

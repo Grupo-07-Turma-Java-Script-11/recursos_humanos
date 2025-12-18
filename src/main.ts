b0e39e526a15d9fbea92573a43116a89ef283417
Daniel Andrade <dan.andrade313@gmail.com>
2025-12-18 13:14:06 -0300

    ajuste no swagger

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
 
   // Indicamos a configuração do Swagger
   const config = new DocumentBuilder()
-    .setTitle('Recursos Humanos')
-    .setDescription('Projeto Integrador')
+    .setTitle('People Flow')
+    .setDescription('Projeto Integrador - Sistema de cadastro de RH')
     .setContact("Grupo 07", "https://github.com/Grupo-07-Turma-Java-Script-11/recursos_humanos", "grupo7javascript11@gmail.com")
     .setVersion('1.0')
     .addBearerAuth()
